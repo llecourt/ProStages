1b534cc7765b7c9fd49325e57784512d371a8756
Leo Lecourt <leo.lecourt@iutbayonne.univ-pau.fr>
2019-11-15 09:10:54 +0100

    modif url page d'accueil

--- src/Controller/ProStagesController.php
+++ src/Controller/ProStagesController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProStagesController extends AbstractController
 {
     /**
-     * @Route("/prostages", name="pro_stages")
+     * @Route("/", name="pro_stages_accueil")
      */
     public function index()
     {
