4c1504b98422ceb2e1c1c6e415d377f803545102
Leo Lecourt <leo.lecourt@iutbayonne.univ-pau.fr>
2019-11-19 13:36:11 +0100

    fin du TP1

--- src/Controller/ProStagesController.php
+++ src/Controller/ProStagesController.php
@@ -12,8 +12,32 @@ class ProStagesController extends AbstractController
      */
     public function index()
     {
-        return $this->render('pro_stages/index.html.twig', [
-            'controller_name' => 'ProStagesController',
-        ]);
+        return $this->render('pro_stages/index.html.twig');
     }
+
+    /**
+     * @Route("/entreprises", name="pro_stages_entreprises")
+     */
+    public function afficherEntreprises()
+    {
+        return $this->render('pro_stages/afficherEntreprises.html.twig');
+    }
+
+    /**
+     * @Route("/formations", name="pro_stages_formations")
+     */
+    public function afficherFormations()
+    {
+        return $this->render('pro_stages/afficherFormations.html.twig');
+    }
+
+    /**
+     * @Route("/stages/{id}", name="pro_stages_stages")
+     */
+    public function afficherStages($id)
+    {
+        return $this->render('pro_stages/afficherStages.html.twig',['id'=>$id]);
+    }
+
+
 }
